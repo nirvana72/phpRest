c5b52702f3d70c6309d377cd5db0004ebee7255e
nirvana72 <15279663@qq.com>
2021-01-13 10:47:55 +0800

    增加一个浏览器输出更友好的dump方法， 来自thinkPhp

--- src/functions.php
+++ src/functions.php
@@ -17,3 +17,30 @@ if (!function_exists('PhpRest\abort')) {
         throw $e;
     }
 }
+
+if (! function_exists ( 'PhpRest\dump' )) {
+    /**
+     * 浏览器友好的变量输出
+     * @param mixed $vars 要输出的变量
+     * @return void
+     */
+    function dump(...$vars)
+    {
+        ob_start();
+        var_dump(...$vars);
+
+        $output = ob_get_clean();
+        $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+
+        if (PHP_SAPI == 'cli') {
+            $output = PHP_EOL . $output . PHP_EOL;
+        } else {
+            if (!extension_loaded('xdebug')) {
+                $output = htmlspecialchars($output, ENT_SUBSTITUTE);
+            }
+            $output = '<pre>' . $output . '</pre>';
+        }
+
+        echo $output;
+    }
+}
