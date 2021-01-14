f49e8f30a40fdc880f9561fc7b2bb2278f16eba1
nirvana72 <15279663@qq.com>
2021-01-14 09:28:04 +0800

    分离example代码

--- App/Controller/Index2Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controller;
-
-class Index2Controller
-{
-    public function index() {
-        return 'OK';
-    }
-}
--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'env' => 'defelop',
-    '\App\Service\*Service' => \DI\create('\App\Service\*Service'),
-];
