358999a7d113ecf8d6f0093cf9f02ead4561e7c8
nirvana72 <15279663@qq.com>
2021-07-23 11:51:47 +0800

    event 增加一个事件名参数

--- src/Event/EventInterface.php
+++ src/Event/EventInterface.php
@@ -5,5 +5,5 @@ interface EventInterface
 {
     public function listen(): array;
 
-    public function handle($params): void;
+    public function handle(string $event, $params): void;
 }
--- src/Event/EventTrigger.php
+++ src/Event/EventTrigger.php
@@ -5,14 +5,16 @@ use PhpRest\Application;
 
 class EventTrigger
 {
-    public static function on($event, $params = [])
+    public static function on(string $event, $params = [])
     {
         $app = Application::getInstance();
         $events = $app->getEvent($event);
         if ($events) {
+            $ary['event'] = $event;
+            $ary['params'] = $params;
             foreach ($events as $classPath) {
                 $ctlClass = $app->get($classPath);
-                call_user_func_array([$ctlClass, 'handle'], $params);
+                call_user_func_array([$ctlClass, 'handle'], $ary);
             }
         }
     }
