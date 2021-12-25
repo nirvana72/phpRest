69cd9fe796fc5eb1a9f6db6cb16f4c65d3529f5d
nirvana72 <15279663@qq.com>
2021-12-26 01:53:00 +0800

    hook如果处于controller同层，支持省略命名空间

--- src/Controller/Annotation/HookHandler.php
+++ src/Controller/Annotation/HookHandler.php
@@ -19,9 +19,20 @@ class HookHandler
      */
     public function __invoke(Controller $controller, AnnotationTag $ann) 
     {
+        // 去除多余空格后切分成数组 1路径 2参数
         $array = explode(' ', trim(preg_replace ( "/\s(?=\s)/","\\1", $ann->description)));
+
+        $classPath = $array[0];
+        // 如果第一位不是 \ 则认为hook位于controller同层命名空间， 否则为绝对路径命名空间
+        // 目前只支持绝对路径命名空间 和 controller同层命名空间
+        if ($classPath[0] !== '\\') {
+            // 根据controller命名空间生成hook命名空间
+            $ctlName = end(explode('\\', $controller->classPath));
+            $classPath = str_replace($ctlName, $classPath, $controller->classPath);
+        }
+
         $hook = new HookMeta();
-        $hook->classPath = $array[0];
+        $hook->classPath = $classPath;
         $hook->params    = $array[1];
 
         is_subclass_of($hook->classPath, HookInterface::class) or \PhpRest\abort(new BadCodeException("{$hook->classPath} 必须继承于 HookInterface"));
